54899e120552ade435a374cacdff127f3dc7b1d3
rcelha <rcelha@gmail.com>
2014-05-09 16:47:21 +0000

    adding plot3

---
+++ plot3.R
@@ -0,0 +1,26 @@
+source("getdata.R")
+
+data <- getdata()
+
+png(filename = "plot3.png",
+    height = 480,
+    width = 480,
+    bg = "transparent")
+
+data$DateAndTime <- strptime(paste(data$Date, data$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S")
+
+
+with(data, {
+  plot(data$DateAndTime, data$Sub_metering_1,
+       type = "l",
+       col = "black",
+       xlab = "",
+       ylab = "Energy sub metering")
+  lines(data$DateAndTime, data$Sub_metering_2, col = "red")
+  lines(data$DateAndTime, data$Sub_metering_3, col = "blue")
+  legend("topright",  lwd = 3, col = c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
+
+
+
+dev.off()
